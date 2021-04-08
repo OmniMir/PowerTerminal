e72d30ce7f04b85488136523cacaea4bf0df691b
kapsilon <kapsilon47@gmail.com>
2021-04-08 21:04:54 +0300

    LittleApps REFACTOR comment headers

--- LittleApps.psm1
+++ LittleApps.psm1
@@ -1,43 +1,42 @@
-# All filenames in current directory without extension
 Function Basenames-OfFiles-InCurrentDirectory {
+##All Filenames in Current Directory without Extension
 	(Get-ChildItem).BaseName
 }
 Set-Alias names -Value Basenames-OfFiles-InCurrentDirectory
 
-# New directories by names from clipboard
 Function NewDirectories-ByNames-InClipboard {
 	$newDirs = Get-Clipboard -Format Text -TextFormatType Text
 	Write-Output $newDirs
 	New-Item -Path ($newDirs) -ItemType directory -Confirm
+##New Directories by Names from Clipboard
 }
 Set-Alias newdirs -Value NewDirectories-ByNames-InClipboard
 
-# Find matching string in file
+##Find Matching String in File
 Function Find-NotUniqueStrings-InFile($file) {
 	$results =
 	Get-Content $file |
 	Group -NoElement |
 	Where { $_.Name -NotLike "" -and $_.Count -NotMatch 1 } |
 	Format-Table -Property Name -AutoSize -HideTableHeaders
-
 	if ($results) { Write-Output $results }
 	else { Write-Output OK }
 }
 Set-Alias uniqs -Value Find-NotUniqueStrings-InFile
 
-# Get full path of symbolic link
+##Get Full Path of Symbolic Link
 Function Get-Full-Path-of-Symbol-Link($path) {
 	Get-Item $path | Select-Object -ExpandProperty Target
 }
 Set-Alias linkpath -Value Get-Full-Path-of-Symbol-Link
 
-# Generate new GUID
 Function Generate-New-GUID() {
+##Generate New GUID
 	[guid]::NewGuid().ToString()
 }
 Set-Alias guid -Value Generate-New-GUID
 
-# Windows grep Command
+##Windows grep Command
 Function Find-As-GREP($target) {
 	#Search string in all files at this location
 	Get-ChildItem -Recurse | Select-String -Pattern $target -Context 1, 1 | ForEach-Object {
@@ -55,7 +54,7 @@ Function Find-As-GREP($target) {
 }
 Set-Alias grep -Value Find-As-GREP
 
-# Interactive-CD
+##Interactive-CD
 Function Interactive-Current-Directory($target) {
 	#Set peco and its parameters as array
 	$pecoExecution = 'C:\Program Files\Peco\peco.exe'
@@ -76,9 +75,9 @@ Function Interactive-Current-Directory($target) {
 }
 Set-Alias cdi -Value Interactive-Current-Directory
 
-# NeoFetch for Windows
+##NeoFetch for Windows
 Set-Alias version -Value "C:\Program Files\WinFetch\winfetch.exe"
 
-# Android Debug Bridge and Fastboot
+##Android Debug Bridge and Fastboot
 Set-Alias adb -Value "C:\Program Files\ADB\adb.exe"
 Set-Alias fastboot -Value "C:\Program Files\ADB\fastboot.exe"
