258341a19b6630629e6471e4689e747560c33793
kapsilon <kapsilon47@gmail.com>
2021-05-17 20:13:43 +0300

    SymLinks REFACTOR

--- SymLinks.psm1
+++ SymLinks.psm1
@@ -1,12 +1,20 @@
-##Create Symbolic Link
+##Create Symbolic Link (Use PowerShell ONLY as ADMINISTRATOR)
 Function Write-SymLink($path, $target) {
-	New-Item -ItemType SymbolicLink -Path $path -Target $target
+	if ($path -and $target) {
+		New-Item -ItemType SymbolicLink -Path $path -Target $target
+	} else {
+		Write-Output "symlink /path/to/link /target/of/link"
+	}
 }
 Set-Alias symlink -Value Write-SymLink
 
 ##Get Full Path of Symbolic Link
 Function Get-Full-Path-of-Symbol-Link($path) {
-	Get-Item $path | Select-Object -ExpandProperty Target
+	if ($path) {
+		Get-Item $path | Select-Object -ExpandProperty Target
+	} else {
+		Write-Output "linkpath /path/to/link"
+	}
 }
 Set-Alias linkpath -Value Get-Full-Path-of-Symbol-Link
 
@@ -25,7 +33,8 @@ Function Convert-SymLinks-to-LNKs($recurse, $path) {
 		$recurse = ""
 	}
 	#Finding all SymLinks in Directory and Subdirectories
-	Get-ChildItem -Attributes ReparsePoint $path $recurse | ForEach-Object -Process {
+	Get-ChildItem -Attributes ReparsePoint $path $recurse |
+	ForEach-Object -Process {
 		#Getting Path and Target
 		$path = $_.FullName
 		$target = [string]$_.Target #Convert to string IMPORTANT
